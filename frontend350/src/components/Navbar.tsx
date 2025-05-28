bfdd06e6bcd2633be6f94e8f80e7a42117f4af42
mdkhaledbin <mdkhaledbin1221@gmail.com>
2025-05-29 01:37:31 +0600

    Added all tthe feature of sidebar

--- frontend350/src/components/Navbar.tsx
+++ frontend350/src/components/Navbar.tsx
@@ -16,7 +16,7 @@ const Navbar = ({ isOpen, setIsOpen }: SideBarProps) => {
 
   return (
     <nav
-      className={`fixed top-0 left-0 w-full flex justify-between items-center h-16 md:h-20 px-4 sm:px-6 lg:px-8 z-50 transition-colors duration-300 ${
+      className={`fixed top-0 left-0 w-full flex justify-between items-center h-16 md:h-20 px-4 sm:px-6 lg:px-8 z-[1010] transition-colors duration-300 ${
         theme === "dark"
           ? "bg-gray-900 border-gray-800"
           : "bg-white border-gray-200"
